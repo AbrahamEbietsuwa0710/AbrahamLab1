0ee36b24d8bd758165174fd10bb544319eaffb8e
AbrahamEbietsuwa0710 <kighimiebietsuwa@gmail.com>
2022-01-23 14:21:59 -0500

    Have a consistent format

---
+++ AbrahamLab1/src/main/java/abraham/ebietsuwa/n01420710/AbrahamActivity.java
@@ -0,0 +1,18 @@
+/*
+  Abraham Ebietsuwa N01420710 RNB
+  Assignment 1
+*/
+package abraham.ebietsuwa.n01420710;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AbrahamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
